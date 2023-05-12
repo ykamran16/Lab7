6bf2ea708a21e2c4fa822e3683546d8b3ce6604f
ykamran16 <ykamran@hawk.iit.edu>
2023-05-12 17:49:20 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function init(){
+//add your javascrip between these two lines of code
+var button = document.getElementById('entrybutton');
+  
+function showMeText(){
+  var textbox = document.getElementById('entryinput');
+  document.getElementById('textoutput').innerHTML = textbox.value;
+  alert("Yawar Kamran: " + textbox.value);
+}
+
+button.addEventListener('click', showMeText);
+}
+
+window.addEventListener('load', init);
